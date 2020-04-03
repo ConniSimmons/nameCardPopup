4363982d7122f603d91a1b5cd81f23968191ab36
connisimmons <connisimmons@gmail.com>
2020-04-03 17:09:36 -0600

    first commit

---
+++ namecard.js
@@ -0,0 +1,16 @@
+
+            
+
+     
+
+function openForm() {
+    document.getElementById("popupForm").style.display="block";
+  }
+  
+  function closeForm() {
+    document.getElementById("popupForm").style.display="none";
+  }
+  function showInput() {
+    document.getElementById('display').innerHTML = 
+    document.getElementById("user_input").value;
+}
